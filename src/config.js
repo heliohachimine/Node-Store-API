2a4f8d327fbf7f47a231597c8f42d811ba17bdf6
heliohachimine <helio_hachimine_jr@hotmail.com>
2018-08-22 14:53:45 -0300

    enviando email de boas vindas

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://heliohachimine:helio123@ds018248.mlab.com:18248/node-str',
-    sendgridkey: 'TBD',
+    sendgridkey: 'SG.RbnJzaMbTRCyYza8S3pY6A.b9Lgy-K3p6Rwl9YQn9-AI9hfTjTvKOYYB2KAryrpgaA',
     containerConnectionString: 'TBD'
 }
